f8467430b885036a7ea373c0f3a05cc5c008aca9
Michael Peyton Jones <michaelpj@gmail.com>
2017-08-09 19:41:11 +0100

    Serve factorio mods for easy access

--- modules/factorio.nix
+++ modules/factorio.nix
@@ -2,6 +2,7 @@
 { config, pkgs, ... }:
 let 
   factorio = "factorio.${domain}";
+  factorio-mods = (pkgs.callPackage ../pkgs/factorio-mods.nix {}).collection;
 in
 {
   nixpkgs.config.allowUnfree = true;
@@ -10,8 +11,17 @@ in
     enable = true;
     game-name = factorio;
     saveName = "railworld";
-    mods = (pkgs.callPackage ../pkgs/factorio-mods.nix {}).collection;
+    mods = factorio-mods;
   };
 
-  services.nginx.virtualHosts."${factorio}".locations."/".proxyPass = "http://localhost:${toString config.services.factorio.port}";
+  services.nginx.virtualHosts."${factorio}" = {
+    locations."/".proxyPass = "http://localhost:${toString config.services.factorio.port}";
+    # serve the mods as a directory
+    locations."/mods" = {
+      # inconveniently replicating what's in the package, but easiest way to
+      # get it out
+      alias = pkgs.factorio-utils.mkModDirDrv factorio-mods;
+      extraConfig = "autoindex on;";
+    };
+  };
 }
