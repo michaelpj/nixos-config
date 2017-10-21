c06b763aacc1e8df12575b6c159ea1ab6b8fa8d5
Michael Peyton Jones <me@michaelpj.com>
2017-10-21 04:40:37 +0100

    Switch to plasma and sddm

--- modules/graphical.nix
+++ modules/graphical.nix
@@ -5,8 +5,8 @@
     enable = true;
     layout = "gb";
     xkbOptions = "caps:escape";
-    desktopManager.xfce.enable = true;
-    displayManager.lightdm.enable = true;
+    desktopManager.plasma5.enable = true;
+    displayManager.sddm.enable = true;
   };
 
   services.redshift = {
@@ -16,10 +16,6 @@
   };
   
   environment.systemPackages = with pkgs; [
-    xfce.xfwm4themes
-    xfce.xfce4_whiskermenu_plugin
-    xfce.xfce4_systemload_plugin
-    xfce.xfce4_pulseaudio_plugin
     numix-gtk-theme numix-icon-theme
   ];
 }
