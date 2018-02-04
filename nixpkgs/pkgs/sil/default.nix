7c35246953870e8706a140eee592ad7206c97d4d
Michael Peyton Jones <me@michaelpj.com>
2018-02-04 22:43:16 +0000

    Actually working sil

--- nixpkgs/pkgs/sil/default.nix
+++ nixpkgs/pkgs/sil/default.nix
@@ -1,38 +1,44 @@
-{ stdenv, fetchzip, ncurses, libX11, libXaw, libXt, libXext, libXmu, writeScriptBin, ... }:
-let sil = stdenv.mkDerivation rec {
+{ stdenv, fetchzip, ncurses, libX11, libXaw, libXt, libXext, libXmu, makeWrapper, writeScript, ... }:
+let 
+  setup = writeScript "setup" ''
+    mkdir -p "$ANGBAND_PATH"
+    # copy all the data files in
+    cp -ar $1/* "$ANGBAND_PATH"
+    # copied files are not writable, make them so
+    chmod +w -R "$ANGBAND_PATH"
+  '';
+in
+stdenv.mkDerivation rec {
   name = "Sil-${version}";
   version = "1.3.0";
 
-  zipname = "Sil-130-src.zip";
   src = fetchzip {
-    url = "http://www.amirrorclear.net/flowers/game/sil/${zipname}";
+    url = "http://www.amirrorclear.net/flowers/game/sil/Sil-130-src.zip";
     sha256 = "1amp2mr3fxascra0k76sdsvikjh8g76nqh46kka9379zd35lfq8w";
-    stripRoot = false;
   };
 
-  buildInputs = [ ncurses libX11 libXaw libXt libXext libXmu ];
+  buildInputs = [ makeWrapper ncurses libX11 libXaw libXt libXext libXmu ];
 
-  sourceRoot = "${zipname}/Sil/src";
+  sourceRoot = "source/Sil/src";
 
   makefile = "Makefile.std";
 
   prePatch = ''
+    # allow usage of ANGBAND_PATH
     substituteInPlace config.h --replace "#define FIXED_PATHS" "" 
   '';
 
   preConfigure = ''
-    buildFlagsArray+=("CFLAGS=-g -fno-strength-reduce -D\"USE_X11\" -D\"USE_GCU\" -D\"PRIVATE_USER_PATH\" -D\"USE_PRIVATE_SAVE_PATH\"")
-    #buildFlagsArray+=("CFLAGS=-O2 -fno-strength-reduce -fomit-frame-pointer -D\"USE_X11\" -D\"USE_TPOSIX\" -D\"USE_GETCH" -D\"USE_CURS_SET\" -D\"USE_GCU\" -D\"PRIVATE_USER_PATH\" -D\"USE_PRIVATE_SAVE_PATH\"")
     buildFlagsArray+=("LIBS=-lXaw -lXext -lSM -lICE -lXmu -lXt -lX11 -lncurses")
   '';
 
   installPhase = ''
-    mkdir $out
-    cp sil $out && cp -r ../lib $out
+    # the makefile doesn't have a sensible install target, so we hav to do it ourselves
+    mkdir -p $out/bin
+    cp sil $out/bin/sil
+    # wrap the program to set a user-local ANGBAND_PATH, and run the setup script to copy files into place
+    wrapProgram $out/bin/sil \
+      --run "export ANGBAND_PATH=\$HOME/.sil" \
+      --run "${setup} ${src}/Sil/lib"
   '';
-};
-in
-writeScriptBin "sil" ''
-  mkdir -p ~/.sil/{scores,save,data}
-  ${sil}/sil -dd=~/.sil/data $@
-''
+}
