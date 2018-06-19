67803e46e5ed06016fabb92a1c496ffddf3bdb33
Michael Peyton Jones <me@michaelpj.com>
2018-06-19 07:57:59 -0400

    Don't bother with modified sddm theme

--- modules/kde.nix
+++ modules/kde.nix
@@ -2,13 +2,6 @@
 {
   services.xserver = {
     desktopManager.plasma5.enable = true;
-    displayManager.sddm = {
-      enable = true;
-      theme = "breeze-modified";
-    };
+    displayManager.sddm.enable = true;
   };
-
-  environment.systemPackages = with pkgs; [
-    (pkgs.callPackage ../nixpkgs/pkgs/sddm/breeze-modified.nix {})
-  ];
 }
