cecd91234e1aaac90bb838995a150bc05a63390d
Michael Peyton Jones <me@michaelpj.com>
2018-08-23 16:01:32 +0100

    Tweaks

--- machines/clipper/configuration.nix
+++ machines/clipper/configuration.nix
@@ -32,23 +32,25 @@ in
     opengl.driSupport32Bit = true;
   };
 
-  services.thermald.enable = true;
-
-  services.xserver.libinput = {
-    enable = true;
-    disableWhileTyping = true;
-  };
-
   boot = {
     loader.systemd-boot.enable = true;
     loader.efi.canTouchEfiVariables = true;
   };
 
+  services = {
+    thermald.enable = true;
+    fstrim.enable = true;
+    xserver.libinput = {
+      enable = true;
+      disableWhileTyping = true;
+    };
+    fwupd.enable = true;
+    fprintd.enable = true;
+  };
+
   # zfs
   boot.supportedFilesystems = [ "zfs" ];
   services.zfs.autoScrub.enable = true;
-
-  services.fprintd.enable = true;
 
   networking = {
     hostName = "clipper"; 
@@ -56,4 +58,45 @@ in
   };
   
   system.stateVersion = "18.09";
+
+  # TODO: move to nixos-hardware
+
+  services.tlp.extraConfig = ''
+    START_CHARGE_THRESH_BAT0=75
+    STOP_CHARGE_THRESH_BAT0=80
+    CPU_SCALING_GOVERNOR_ON_BAT=powersave
+    ENERGY_PERF_POLICY_ON_BAT=powersave
+  '';
+
+  systemd.services.cpu-throttling = {
+    enable = true;
+    description = "Sets the offset to 3 °C, so the new trip point is 97 °C";
+    documentation = [
+      "https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Power_management.2FThrottling_issues"
+    ];
+    path = [ pkgs.msr-tools ];
+    script = "wrmsr -a 0x1a2 0x3000000";
+    serviceConfig = {
+      Type = "oneshot";
+    };
+    wantedBy = [
+      "timers.target"
+    ];
+  };
+
+  systemd.timers.cpu-throttling = {
+    enable = true;
+    description = "Set cpu heating limit to 97 °C";
+    documentation = [
+      "https://wiki.archlinux.org/index.php/Lenovo_ThinkPad_X1_Carbon_(Gen_6)#Power_management.2FThrottling_issues"
+    ];
+    timerConfig = {
+      OnActiveSec = 60;
+      OnUnitActiveSec = 60;
+      Unit = "cpu-throttling.service";
+    };
+    wantedBy = [
+      "timers.target"
+    ];
+};
 }
