d0905a4aea7a5e8b85f4dcb95b14a472650b5e42
Michael Peyton Jones <me@michaelpj.com>
2018-08-26 17:29:33 +0100

    Maybe redundant extra namservers setting

--- modules/networking.nix
+++ modules/networking.nix
@@ -2,9 +2,15 @@
 
 {
   networking = {
-    networkmanager.enable = true;
+    networkmanager = { 
+      enable = true;
+      # I am confused about whether I need this as well as
+      # networking.nameservers
+      appendNameservers = [ "8.8.8.8" ]; 
+    };
     firewall.enable = false;
     nameservers = [ "8.8.8.8" ];
+
   };
 
   services.avahi = {
