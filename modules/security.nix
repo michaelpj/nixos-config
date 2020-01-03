1b05b4314c8701c43f4f983250adc824d06f5dfa
Michael Peyton Jones <me@michaelpj.com>
2020-01-03 12:11:27 +0000

    One more thing

--- modules/security.nix
+++ modules/security.nix
@@ -1,11 +1,14 @@
 { config, pkgs, ... }:
 
 {
+  # enable u2f
   hardware.u2f.enable = true;
 
+  # enable smart card reader driver
   services.pcscd.enable = true;
 
   # yubikey stuff
+  services.udev.packages = [ pkgs.yubikey-personalization ];
   environment.systemPackages = [
     pkgs.yubikey-personalization
     pkgs.yubikey-personalization-gui
