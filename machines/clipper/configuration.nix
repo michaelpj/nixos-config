d21382fe0cb39e9434b79196ff2b8ba6e0696dc0
Michael Peyton Jones <me@michaelpj.com>
2020-08-26 10:44:07 +0100

    update tlp

--- machines/clipper/configuration.nix
+++ machines/clipper/configuration.nix
@@ -82,10 +82,8 @@
 
   # TODO: move to nixos-hardware
 
-  services.tlp.extraConfig = ''
-    START_CHARGE_THRESH_BAT0=75
-    STOP_CHARGE_THRESH_BAT0=80
-    CPU_SCALING_GOVERNOR_ON_BAT=powersave
-    ENERGY_PERF_POLICY_ON_BAT=powersave
-  '';
+  services.tlp.settings = {
+    START_CHARGE_THRESH_BAT0 = 75;
+    STOP_CHARGE_THRESH_BAT0 = 80;
+  };
 }
