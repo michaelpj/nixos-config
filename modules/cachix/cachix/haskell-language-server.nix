e18409277955a582661a7942bd841290a200aefb
Michael Peyton Jones <me@michaelpj.com>
2021-09-17 10:13:08 +0100

    Cachix

---
+++ modules/cachix/cachix/haskell-language-server.nix
@@ -0,0 +1,11 @@
+
+{
+  nix = {
+    binaryCaches = [
+      "https://haskell-language-server.cachix.org"
+    ];
+    binaryCachePublicKeys = [
+      "haskell-language-server.cachix.org-1:juFfHrwkOxqIOZShtC4YC1uT1bBcq2RSvC7OMKx0Nz8="
+    ];
+  };
+}
