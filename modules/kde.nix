e61139a731c4f708b5956b6fd848792c3a4f6737
Michael Peyton Jones <me@michaelpj.com>
2024-03-07 08:50:41 +0000

    Plasma 6

--- modules/kde.nix
+++ modules/kde.nix
@@ -1,7 +1,7 @@
 { config, pkgs, ... }:
 {
   services.xserver = {
-    desktopManager.plasma5.enable = true;
+    desktopManager.plasma6.enable = true;
     displayManager.sddm = {
       enable = true;
       enableHidpi = true;
