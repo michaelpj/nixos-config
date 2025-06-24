e9887894f4be83834fd06b3edfe4c328e5e3f31c
Michael Peyton Jones <me@michaelpj.com>
2025-06-24 14:36:45 +0100

    updates

--- nixpkgs/overlays/updates.nix
+++ nixpkgs/overlays/updates.nix
@@ -1,11 +1,17 @@
 final: prev: {
-  mergiraf = prev.mergiraf.overrideAttrs (drv: {
-		src = fetchFromGitea {
+  mergiraf = prev.mergiraf.overrideAttrs (finalAttrs: prevAttrs: rec {
+    version = "0.10.0";
+		src = prev.fetchFromGitea {
 			domain = "codeberg.org";
 			owner = "mergiraf";
 			repo = "mergiraf";
 			rev = "174ee87c9bd9de77ef824b7797d9df7b418c65fb";
-			hash = "sha256-vnXOl7KzSvvxQP4CebOJ+fEIn7fQDKTmO2PkGMRA4t4=";
+			hash = "sha256-OvV48s9Us/GaV4H+x0Qx+ZX85L+4fjfx1+gPqg3BR7A=";
+		};
+    cargoHash = "sha256-KCGX0QYnedoj3BTq+3pGXxjefPwhbbCd5q5auIvVWZ4=";
+		cargoDeps = prev.rustPlatform.fetchCargoVendor {
+			inherit (finalAttrs) pname src version;
+			hash = finalAttrs.cargoHash;
 		};
   });
 }
